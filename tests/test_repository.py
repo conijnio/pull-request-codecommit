conijnio/pull-request-codecommit

commit 52389cc9e2a069b140ee87559378a6c3e753bf56

fix: support fallback to default profile and region (#11)
* fix: support fallback to default profile and region

By moving the remote logic to its own class it allows us to encapsulate the behaviour. And making it easier to extend the behaviour.
Support both `codecommit://` and `codecommit::://` notations.

Fixes issue #7

pull_request_codecommit/git/remote.py
@@ -1,0 +1,50 @@
+import re
+from typing import Optional
+
+
+class Remote:
+    """
+    Understands remote CodeCommit URLs
+    """
+
+    def __init__(self, url: str):
+        self.__url: str = url
+        self.__region: Optional[str] = ""
+        self.__profile: Optional[str] = ""
+        self.__name: str = ""
+
+    def __regex(self, pattern: str, index: int = 1) -> Optional[str]:
+        match = re.search(pattern, self.__url)
+        return match.group(index) if match else None
+
+    @property
+    def supported(self) -> bool:
+        return self.__url.startswith("codecommit:") and self.name != ""
+
+    @property
+    def url(self) -> str:
+        return self.__url
+
+    @property
+    def region(self) -> Optional[str]:
+        if not self.__region:
+            self.__region = self.__regex(r"^codecommit::(.*)://")
+
+        return self.__region
+
+    @property
+    def profile(self) -> Optional[str]:
+        if self.__profile == "":
+            self.__profile = self.__regex(r"//(.*)@")
+
+        return self.__profile
+
+    @property
+    def name(self) -> str:
+        if not self.__name:
+            name = self.__regex(r"(\/\/|@)(.*)$", 2)
+
+            if name:
+                self.__name = name
+
+        return self.__name

pull_request_codecommit/__init__.py
@@ -13,13 +13,13 @@ def main(repository_path: Optional[str]) -> None:
     """
     repo = Repository(repository_path)
 
-    if not repo.supported:
+    if not repo.remote.supported:
         raise click.ClickException("The repository is not compatible with this tool!")
 
-    click.echo(f"Remote: {repo.remote}")
-    click.echo(f"Region: {repo.aws_region}")
-    click.echo(f"Profile: {repo.aws_profile}")
-    click.echo(f"Repo: {repo.repository_name}")
+    click.echo(f"Remote: {repo.remote.url}")
+    click.echo(f"Region: {repo.remote.region}")
+    click.echo(f"Profile: {repo.remote.profile}")
+    click.echo(f"Repo: {repo.remote.name}")
     click.echo(f"Branch: {repo.branch}")
     click.echo()
     pull_request = repo.pull_request_information()

pull_request_codecommit/aws/client.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import json
-from typing import List
+from typing import List, Optional
 import subprocess
 
 
@@ -12,12 +12,25 @@ class Client:
     We use the AWS CLI for these operations so that we can use tha MFA toke cache from the cli.
     """
 
-    def __init__(self, profile: str, region: str) -> None:
-        self.__profile = profile
-        self.__region = region
+    def __init__(self, profile: Optional[str], region: Optional[str]) -> None:
+        self.__base_command: List[str] = []
+        self.__profile: Optional[str] = profile
+        self.__region: Optional[str] = region
+
+    @property
+    def base_command(self) -> List[str]:
+        base_command = ["aws"]
+
+        if self.__profile:
+            base_command.extend(["--profile", self.__profile])
+
+        if self.__region:
+            base_command.extend(["--region", self.__region])
+
+        return base_command
 
     def __execute(self, parameters: List[str]) -> str:
-        command = ["aws", "--profile", self.__profile, "--region", self.__region]
+        command = self.base_command
         command.extend(parameters)
         response = subprocess.run(command, stdout=subprocess.PIPE)
 

pull_request_codecommit/git/__init__.py
@@ -1,4 +1,5 @@
-from .commits import Commits
-from .commit import Commit
-from .message import Message
 from .client import Client
+from .commit import Commit
+from .commits import Commits
+from .message import Message
+from .remote import Remote

pull_request_codecommit/repository.py
@@ -1,9 +1,8 @@
 import os
-import re
-from typing import Optional, Tuple
+from typing import Optional
 
 from .config import Config
-from .git import Client as GitClient
+from .git import Client as GitClient, Remote
 from .aws import Client as AwsClient
 from .pull_request import PullRequest
 
@@ -14,10 +13,7 @@ class Repository:
     """
 
     def __init__(self, path: Optional[str] = None) -> None:
-        self.__remote: str = ""
-        self.__aws_region: str = ""
-        self.__aws_profile: str = ""
-        self.__repository_name: str = ""
+        self.__remote: Optional[Remote] = None
         self.__branch: str = ""
 
         if not path:
@@ -26,43 +22,14 @@ class Repository:
         self.__git = GitClient(path)
 
     def __config(self, method: str) -> Optional[str]:
-        return getattr(Config, method)(self.aws_profile)
+        return getattr(Config, method)(self.remote.profile)
 
     @property
-    def supported(self) -> bool:
-        return self.remote.startswith("codecommit::")
-
-    @property
-    def remote(self) -> str:
+    def remote(self) -> Remote:
         if not self.__remote:
-            self.__remote = self.__git.remote("origin")
-
-            if self.supported:
-                self.__extract_from_remote(self.__remote)
+            self.__remote = Remote(self.__git.remote("origin"))
 
         return self.__remote
-
-    def __extract_from_remote(self, remote: str) -> None:
-        def resolve(resolver: Optional[re.Match], index: int) -> str:
-            return resolver.group(index) if resolver else ""
-
-        # This can use some more error handling
-        match = re.search(r"^codecommit::(.*)://(.*)@(.*)$", remote)
-        self.__aws_region = resolve(match, 1)
-        self.__aws_profile = resolve(match, 2)
-        self.__repository_name = resolve(match, 3)
-
-    @property
-    def aws_region(self) -> str:
-        return self.__aws_region
-
-    @property
-    def aws_profile(self) -> str:
-        return self.__aws_profile
-
-    @property
-    def repository_name(self) -> str:
-        return self.__repository_name
 
     @property
     def branch(self) -> str:
@@ -81,12 +48,12 @@ class Repository:
         return PullRequest(commits)
 
     def create_pull_request(self, title: str, description: str) -> str:
-        client = AwsClient(profile=self.__aws_profile, region=self.__aws_region)
+        client = AwsClient(profile=self.remote.profile, region=self.remote.region)
         response = client.create_pull_request(
             title=title,
             description=description,
-            repository=self.repository_name,
+            repository=self.remote.name,
             source=self.branch,
             destination=self.destination,
         )
-        return f"https://{self.__aws_region}.console.aws.amazon.com/codesuite/codecommit/repositories/{self.repository_name}/pull-requests/{response.get('pullRequestId')}/details?region={self.__aws_region}"
+        return f"https://{self.remote.region}.console.aws.amazon.com/codesuite/codecommit/repositories/{self.remote.name}/pull-requests/{response.get('pullRequestId')}/details?region={self.remote.region}"

tests/test_command.py
@@ -15,29 +15,68 @@ def edit_message(message: str) -> str:
 
 
 @pytest.mark.parametrize(
-    "region, profile, config, commits",
+    "remote, region, profile, config, commits",
     [
         (
+            "codecommit::eu-west-1://my-profile@my-repository",
             "eu-west-1",
             "my-profile",
             b"[default]\nbranch: my-main\n[profile my-profile]\nbranch: my-master",
             COMMITS,
         ),
         (
+            "codecommit::eu-west-1://my-profile@my-repository",
             "eu-central-1",
             "my-profile",
             b"[default]\nbranch: my-main\n[profile my-profile]\nbranch: my-master",
             COMMITS_NO_ISSUES,
         ),
         (
+            "codecommit::eu-west-1://my-profile@my-repository",
             "eu-west-1",
             "my-other-profile",
             b"[default]\nbranch: my-main\n[profile my-profile]\nbranch: my-master",
             COMMITS_NO_ISSUES,
         ),
         (
+            "codecommit::eu-west-1://my-profile@my-repository",
             "eu-central-1",
             "my-other-profile",
+            b"[default]\nbranch: my-main\n[profile my-profile]\nbranch: my-master",
+            COMMITS,
+        ),
+        (
+            "codecommit::eu-west-1://my-repository",
+            "eu-central-1",
+            None,
+            b"[default]\nbranch: my-main\n[profile my-profile]\nbranch: my-master",
+            COMMITS,
+        ),
+        (
+            "codecommit::://my-profile@my-repository",
+            None,
+            "my-profile",
+            b"[default]\nbranch: my-main\n[profile my-profile]\nbranch: my-master",
+            COMMITS,
+        ),
+        (
+            "codecommit::://my-repository",
+            None,
+            None,
+            b"[default]\nbranch: my-main\n[profile my-profile]\nbranch: my-master",
+            COMMITS,
+        ),
+        (
+            "codecommit://my-profile@my-repository",
+            None,
+            "my-profile",
+            b"[default]\nbranch: my-main\n[profile my-profile]\nbranch: my-master",
+            COMMITS,
+        ),
+        (
+            "codecommit://my-repository",
+            None,
+            None,
             b"[default]\nbranch: my-main\n[profile my-profile]\nbranch: my-master",
             COMMITS,
         ),
@@ -50,6 +89,7 @@ def test_invoke(
     mock_edit: MagicMock,
     mock_git_client: MagicMock,
     mock_aws_client: MagicMock,
+    remote: str,
     region: str,
     profile: str,
     config: bytes,
@@ -58,9 +98,7 @@ def test_invoke(
     mock_edit.side_effect = edit_message
     mock_git_client.return_value.get_commit_messages.return_value = Commits(commits)
 
-    mock_git_client.return_value.remote.return_value = (
-        f"codecommit::{region}://{profile}@my-repository"
-    )
+    mock_git_client.return_value.remote.return_value = remote
     mock_git_client.return_value.current_branch.return_value = "feat/my-feature"
     configparser.open = MagicMock(return_value=TextIOWrapper(BytesIO(config)))  # type: ignore
 
@@ -186,3 +224,18 @@ def test_invoke_quit_edit(
     runner = CliRunner()
     result = runner.invoke(main)
     assert result.exit_code == 1
+
+
+@patch("pull_request_codecommit.repository.GitClient")
+def test_invoke_no_repository_name(
+    mock_git_client: MagicMock,
+) -> None:
+    mock_git_client.return_value.remote.return_value = f"codecommit::eu-west-1://"
+    mock_git_client.return_value.current_branch.return_value = "feat/my-feature"
+    config = b"[default]\nbranch: my-main\n[profile my-profile]\nbranch: my-master"
+    configparser.open = MagicMock(return_value=TextIOWrapper(BytesIO(config)))  # type: ignore
+
+    runner = CliRunner()
+    result = runner.invoke(main)
+    assert result.exit_code == 1
+    assert "Error: The repository is not compatible with this tool!" in result.output

commit 61d54ce2f94e16d0bcaf1eec6395804828f8ff53

fix: repo name remote contains profile
When the remote contains a profile it was appended to the repository name. This was caused by #11.

Issue: #11

pull_request_codecommit/git/remote.py
@@ -42,7 +42,7 @@ class Remote:
     @property
     def name(self) -> str:
         if not self.__name:
-            name = self.__regex(r"(\/\/|@)(.*)$", 2)
+            name = self.__regex(r"(\/\/|.*@)(.*)$", 2)
 
             if name:
                 self.__name = name

tests/test_repository.py
@@ -1,0 +1,23 @@
+import pytest
+from unittest.mock import patch, MagicMock
+from pull_request_codecommit.repository import Repository
+
+
+@pytest.mark.parametrize(
+    "region, profile, repository",
+    [
+        ("eu-west-1", "my-profile", "my-repository"),
+        ("eu-central-1", "my-other-profile", "my-other-repository"),
+    ],
+)
+@patch("pull_request_codecommit.repository.GitClient")
+def test_remote(
+    mock_git_client: MagicMock, region: str, profile: str, repository: str
+) -> None:
+    mock_git_client.return_value.remote.return_value = (
+        f"codecommit::{region}://{profile}@{repository}"
+    )
+    repo = Repository("/my/repository")
+    assert repo.remote.region == region
+    assert repo.remote.profile == profile
+    assert repo.remote.name == repository