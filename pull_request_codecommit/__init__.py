16c04213e0cabb86cea20cd2f097941c36fd40eb
Joris Conijn <jorisconijn@binx.io>
2022-02-05 10:13:22 +0100

    refactor: prepare codebase for auto-merge feature (#19)
    * refactor: simplify the cli interaction
    * refactor: encapsulate pull request information
    * refactor: move pull request creation

--- pull_request_codecommit/__init__.py
+++ pull_request_codecommit/__init__.py
@@ -2,7 +2,8 @@ from typing import Optional
 
 import click
 
-from pull_request_codecommit.repository import Repository
+from .pull_request import PullRequest
+from .repository import Repository
 
 
 @click.command()
@@ -11,37 +12,59 @@ def main(repository_path: Optional[str]) -> None:
     """
     pull-request-codecommit
     """
+    repo = __load_repository(repository_path)
+    __display_repository_information(repo)
+    __create_pull_request(repo)
+
+
+def __load_repository(repository_path: Optional[str]) -> Repository:
     repo = Repository(repository_path)
 
     if not repo.remote.supported:
         raise click.ClickException("The repository is not compatible with this tool!")
 
+    return repo
+
+
+def __create_pull_request(repo: Repository) -> PullRequest:
+    pr = PullRequest(repo)
+
+    if not pr.has_changes:
+        click.echo(
+            f"There are no differences between {repo.destination} and {repo.branch}!"
+        )
+        exit(0)
+
+    __propose_title_description(pr)
+    pr.create()
+
+    click.echo(f"Please review/approve: {pr.link}")
+    click.echo()
+    click.echo(click.style(pr.title, bold=True))
+    click.echo(pr.description)
+
+    return pr
+
+
+def __display_repository_information(repo: Repository) -> None:
     click.echo(f"Remote: {repo.remote.url}")
     click.echo(f"Region: {repo.remote.region}")
     click.echo(f"Profile: {repo.remote.profile}")
     click.echo(f"Repo: {repo.remote.name}")
     click.echo(f"Branch: {repo.branch}")
     click.echo()
-    pull_request = repo.pull_request_information()
 
-    if not pull_request.has_changes:
-        click.echo(
-            f"There are no differences between {repo.destination} and {repo.branch}!"
-        )
-        exit(0)
 
-    message = click.edit(f"{pull_request.title}\n\n{pull_request.description}")
+def __propose_title_description(pr: PullRequest) -> None:
+    message = click.edit(f"{pr.title}\n\n{pr.description}")
 
     if message is None:
         raise click.ClickException("Pull request was not created")
 
     title = message.splitlines()[0]
     description = "\n".join(message.splitlines()[1:]).lstrip("\n")
-    link = repo.create_pull_request(title, description)
-    click.echo(f"Please review/approve: {link}")
-    click.echo()
-    click.echo(click.style(title, bold=True))
-    click.echo(description)
+
+    pr.update(title, description)
 
 
 if __name__ == "__main__":
--- pull_request_codecommit/pull_request.py
+++ pull_request_codecommit/pull_request.py
@@ -1,4 +1,6 @@
+from .repository import Repository
 from pull_request_codecommit.git import Commits
+from .aws import Client as AwsClient
 
 
 class PullRequest:
@@ -6,8 +8,38 @@ class PullRequest:
     Understands pull requests
     """
 
-    def __init__(self, commits: Commits) -> None:
-        self.__commits = commits
+    def __init__(self, repo: Repository) -> None:
+        self.__repo: Repository = repo
+        self.__commits: Commits = self.__repo.commits()
+        self.__title: str = ""
+        self.__description: str = ""
+        self.__link: str = ""
+
+    def update(self, title: str, description: str) -> None:
+        self.__title = title
+        self.__description = description
+
+    def create(self) -> None:
+        self.__repo.push()
+        client = AwsClient(
+            profile=self.__repo.remote.profile, region=self.__repo.remote.region
+        )
+        response = client.create_pull_request(
+            title=self.title,
+            description=self.description,
+            repository=self.__repo.remote.name,
+            source=self.__repo.branch,
+            destination=self.__repo.destination,
+        )
+
+        self.__link = "".join(
+            [
+                f"https://{self.__repo.remote.region}.console.aws.amazon.com/",
+                "codesuite/codecommit/repositories/",
+                f"pull-requests/{response.get('pullRequestId')}/details",
+                f"?region={self.__repo.remote.region}",
+            ]
+        )
 
     @property
     def has_changes(self) -> bool:
@@ -15,6 +47,9 @@ class PullRequest:
 
     @property
     def title(self) -> str:
+        if self.__title:
+            return self.__title
+
         if not self.__commits.first:
             return ""
 
@@ -27,6 +62,9 @@ class PullRequest:
 
     @property
     def description(self) -> str:
+        if self.__description:
+            return self.__description
+
         if len(self.__commits) == 1:
             description = (
                 [self.__commits.first.message.body] if self.__commits.first else []
@@ -40,3 +78,7 @@ class PullRequest:
             description.append(f"\nIssues: " + ", ".join(self.__commits.issues))
 
         return "\n".join(description).lstrip("\n")
+
+    @property
+    def link(self) -> str:
+        return self.__link
